63b1eca17f22cc31d4ec547777b8db683e75bfe2
sunnykumar31 <jaissunny158@gmail.com>
2022-07-25 20:43:49 +0530

    code changes

--- script.js
+++ script.js
@@ -43,7 +43,3 @@ var next=document.querySelector(".next");
             }
     
             window.onload=automove;
-
-
-            // var box1=document.getElementsByClassName(".Box1");
-            // box1.onclick()
